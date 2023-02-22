299a4eb378fe4813de2be67244159e518847b5ce
javad <javad.feyzabadi@yahoo.com>
2023-02-23 02:33:41 +0330

    start sender

---
+++ sender.py
@@ -0,0 +1,7 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+ch1 = connection.channel()
+ch1.queue_declare(queue='hello')
+ch1.basic_publish(exchange='',routing_key='hello',body='Hello World')
+connection.close()
